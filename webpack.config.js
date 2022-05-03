feb6444ba5698f5a3ebc258f83c845acb1311b39
Ian <s5354555@gmail.com>
2022-05-03 09:40:40 +0000

    init ver.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+module.exports = {
+    mode: "development",
+    entry:"./src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public")
+    },
+    devServer: {
+        static: "./public"
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    }
+}
